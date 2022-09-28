5ec0c4cbf17ef4b9fb943a0a8f127eab7b51e94f
R1senDev <ris3nanderson@gmail.com>
2022-09-28 12:17:46 +0300

    Removed seed for randomizer

--- neuro.py
+++ neuro.py
@@ -6,8 +6,6 @@ def sigmoid(x):
 training_inputs = np.array([[0,0,1], [1,1,1], [1,0,1], [0,1,1]])
 training_outputs = np.array([[0,1,1,0]]).T
 
-np.random.seed(1)
-
 synaptic_weights = 2 * np.random.random((3,1)) - 1
 
 print("Случайные инициализирующие веса:")
